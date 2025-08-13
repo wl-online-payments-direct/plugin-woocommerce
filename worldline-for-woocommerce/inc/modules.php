61fbfce1c0704acfde1b18dc738cf94a6e25d621
Worldline-plugins-1 <dev+WorldlinePlugins@logeecom.com>
2025-08-13 14:17:08 +0200

    Release 2.3.0

--- worldline-for-woocommerce/inc/modules.php
+++ worldline-for-woocommerce/inc/modules.php
@@ -1,69 +1,36 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types=1);
+namespace Syde\Vendor\Worldline;
 
-use Inpsyde\Logger\LoggerModule;
-use Inpsyde\Modularity\Module\Module;
-use Inpsyde\PaymentGateway\PaymentGatewayModule;
-use Inpsyde\WorldlineForWoocommerce\ApplePayGateway\ApplePayGatewayModule;
-use Inpsyde\WorldlineForWoocommerce\BankTransferGateway\BankTransferGatewayModule;
-use Inpsyde\WorldlineForWoocommerce\Checkout\CheckoutModule;
-use Inpsyde\WorldlineForWoocommerce\Config\ConfigModule;
-use Inpsyde\WorldlineForWoocommerce\Core\CoreModule;
 use Inpsyde\WorldlineForWoocommerce\CVCOGateway\CVCOGatewayModule;
-use Inpsyde\WorldlineForWoocommerce\Documentation\DocumentationModule;
-use Inpsyde\WorldlineForWoocommerce\Environment\EnvironmentModule;
-use Inpsyde\WorldlineForWoocommerce\GooglePayGateway\GooglePayGatewayModule;
-use Inpsyde\WorldlineForWoocommerce\HostedTokenizationGateway\HostedTokenizationGatewayModule;
-use Inpsyde\WorldlineForWoocommerce\IdealGateway\IdealGatewayModule;
 use Inpsyde\WorldlineForWoocommerce\MealvouchersGateway\MealvouchersGatewayModule;
-use Inpsyde\WorldlineForWoocommerce\PostfinanceGateway\PostfinanceGatewayModule;
-use Inpsyde\WorldlineForWoocommerce\KlarnaGateway\KlarnaGatewayModule;
 use Inpsyde\WorldlineForWoocommerce\ProductType\ProductTypeModule;
-use Inpsyde\WorldlineForWoocommerce\ReturnPage\ReturnPageModule;
-use Inpsyde\WorldlineForWoocommerce\TwintGateway\TwintGatewayModule;
-use Inpsyde\WorldlineForWoocommerce\Uninstall\UninstallModule;
-use Inpsyde\WorldlineForWoocommerce\Uri\UriModule;
-use Inpsyde\WorldlineForWoocommerce\Utils\UtilsModule;
-use Inpsyde\WorldlineForWoocommerce\Vaulting\VaultingModule;
-use Inpsyde\WorldlineForWoocommerce\WcSupport\WcSupportModule;
-use Inpsyde\WorldlineForWoocommerce\Webhooks\WebhooksModule;
-use Inpsyde\WorldlineForWoocommerce\WorldlineLogging\WorldlineLoggingModule;
-use Inpsyde\WorldlineForWoocommerce\WorldlinePaymentGateway\WorldlinePaymentGatewayModule;
-use Inpsyde\WorldlineForWoocommerce\EpsGateway\EpsGatewayModule;
-
-return /**
- * @return iterable<Module>
- */
-    static function (): iterable {
-        return [
-            new EnvironmentModule(),
-            new CoreModule(),
-            new PaymentGatewayModule(),
-            new LoggerModule(),
-            new WorldlineLoggingModule(),
-            new UriModule(),
-            new WcSupportModule(),
-            new ConfigModule(),
-            new WorldlinePaymentGatewayModule(),
-            new HostedTokenizationGatewayModule(),
-            new GooglePayGatewayModule(),
-            new ApplePayGatewayModule(),
-            new BankTransferGatewayModule(),
-            new IdealGatewayModule(),
-            new EpsGatewayModule(),
-            new PostfinanceGatewayModule(),
-            new KlarnaGatewayModule(),
-            new TwintGatewayModule(),
-            new CheckoutModule(),
-            new ReturnPageModule(),
-            new WebhooksModule(),
-            new VaultingModule(),
-            new UtilsModule(),
-            new DocumentationModule(),
-            new UninstallModule(),
-            new MealvouchersGatewayModule(),
-            new ProductTypeModule(),
-            new CVCOGatewayModule(),
-        ];
-    };
+use Syde\Vendor\Worldline\Inpsyde\Logger\LoggerModule;
+use Syde\Vendor\Worldline\Inpsyde\Modularity\Module\Module;
+use Syde\Vendor\Worldline\Inpsyde\PaymentGateway\PaymentGatewayModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\ApplePayGateway\ApplePayGatewayModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\BankTransferGateway\BankTransferGatewayModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\Checkout\CheckoutModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\Config\ConfigModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\Core\CoreModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\Documentation\DocumentationModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\Environment\EnvironmentModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\GooglePayGateway\GooglePayGatewayModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\HostedTokenizationGateway\HostedTokenizationGatewayModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\IdealGateway\IdealGatewayModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\PostfinanceGateway\PostfinanceGatewayModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\KlarnaGateway\KlarnaGatewayModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\ReturnPage\ReturnPageModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\TwintGateway\TwintGatewayModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\Uninstall\UninstallModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\Uri\UriModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\Utils\UtilsModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\Vaulting\VaultingModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\WcSupport\WcSupportModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\Webhooks\WebhooksModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\WorldlineLogging\WorldlineLoggingModule;
+use Syde\Vendor\Worldline\Inpsyde\WorldlineForWoocommerce\WorldlinePaymentGateway\WorldlinePaymentGatewayModule;
+return static function (): iterable {
+    return [new EnvironmentModule(), new CoreModule(), new PaymentGatewayModule(), new LoggerModule(), new WorldlineLoggingModule(), new UriModule(), new WcSupportModule(), new ConfigModule(), new WorldlinePaymentGatewayModule(), new HostedTokenizationGatewayModule(), new GooglePayGatewayModule(), new ApplePayGatewayModule(), new BankTransferGatewayModule(), new IdealGatewayModule(), new PostfinanceGatewayModule(), new KlarnaGatewayModule(), new TwintGatewayModule(), new CheckoutModule(), new ReturnPageModule(), new WebhooksModule(), new VaultingModule(), new UtilsModule(), new DocumentationModule(), new UninstallModule(), new MealvouchersGatewayModule(), new ProductTypeModule(), new CVCOGatewayModule()];
+};
